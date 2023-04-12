b7fe1da9e4de3683a079bb053dfea7498eb1690a
iSaru <111219643+Drachentoter@users.noreply.github.com>
2023-04-12 11:11:38 +0700

    satu

---
+++ Satu.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+//comment
+/* multi
+line
+comment
+*/
+
+int main() {
+    
+    cout << "Satu" << endl;
+    cout << "Dua \n";
+    cout << "tiga ";
+    cout << "empat" << endl;
+    cout << "lima" << endl;
+
+    int a = 20;
+    int b = 30;
+    cout << a + b << endl;
+
+    int c;
+    int d;
+    c = 10;
+    d = 10;
+    cout << c + d << endl;
+
+    int e;
+    cout << "masukkan nilai: " << endl;
+    cin >> e ;
+    cout << "nilai yang telah anda masukkan adalah: ";
+    cout << e << endl;
+
+    
+
+    cin.get();
+    return 0;
+}

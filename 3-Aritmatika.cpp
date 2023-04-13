0c524988a18ea9d5b65f7825d7afdba70b053c48
iSaru <tetprogramming@gmail.com>
2023-04-13 07:52:39 +0700

    aritmatika

--- 3-Aritmatika.cpp
+++ 3-Aritmatika.cpp
@@ -10,21 +10,27 @@ int main() {
     float hasil;
     // operator nya ada +, -, *, /, %
 
+    // penambahan
     hasil = a + b;
     cout << a << " + " << b << " = " << hasil << endl;
 
+    // pengurangan
     hasil = a - b;
     cout << a << " - " << b << " = " << hasil << endl;
 
+    // perkalian
     hasil = a * b;
     cout << a << " x " << b << " = " << hasil << endl;
 
+    // pembagian
     hasil = a / c;
     cout << a << " / " << c << " = " << hasil << endl;
 
+    // modulus
     hasil = a % b;
     cout << a << " % " << c << " = " << hasil << endl;
 
+    // urutan eksekusi
     hasil = (a + b) * c;
     cout << "(" << a << " + " << b << ")" << " x " << c << " = " << hasil << endl;
 

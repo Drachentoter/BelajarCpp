4b3fc481cf03f06478ad93da6370c20aca4e2b3f
iSaru <tetprogramming@gmail.com>
2023-04-13 09:25:41 +0700

    if statement

---
+++ 6-If.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int a;
+    cout << "masukan angka: " << endl;
+    cin >> a;
+
+    if (a > 3) // if sama kek boolean jadi kamu bisa menambahkan sama dengan atau lebih dari dll
+    {
+        cout << "domoooo" << endl;
+    }
+    cout << "selesai" << endl;
+
+    cin.get();
+    return 0;
+}
